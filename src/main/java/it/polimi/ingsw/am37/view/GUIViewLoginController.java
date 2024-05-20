62f6b8a57a2ff16be481d889cb61559c1c86bebf
Alienn717 <alberto.brignola@mail.polimi.it>
2024-05-20 15:11:17 +0200

    prima view GUI per login, solo per capire come funziona

---
+++ src/main/java/it/polimi/ingsw/am37/view/GUIViewLoginController.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.am37.view;
+
+public class GUIViewLoginController {
+
+    public void createButtonClicked(){
+        System.out.println("create");
+    }
+    public void joinButtonClicked(){
+        System.out.println("join");
+    }
+}
